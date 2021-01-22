769315c38a93596111c66407bf648606fe103c31
Cloudbop <boppincolin@gmail.com>
2021-01-22 16:28:59 +0000

    +Display.spec.js and added failing render test

---
+++ src/components/Display/Display.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Display from './Display'
+
+describe('Calculator', () => {
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<Display />));
+  // it('Should compare against snapshot <Calculator/>', () => expect(wrapper).toMatchSnapshot())
+  
+  it('should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  
+});
