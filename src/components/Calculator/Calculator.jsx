ae1c3a70206c4145852a018036b8fd86329feb63
Cloudbop <boppincolin@gmail.com>
2021-01-22 23:40:03 +0000

    PASSTESTS:Calculator.spec updateDisplay fns

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -3,6 +3,7 @@ import './Calculator.css'
 import Display from '../Display/Display'
 import Keypad from '../Keypad/Keypad'
 class Calculator extends Component {
+  // see note*
   state = {
     // value to be displayed in <Display />
     displayValue: '0',
@@ -24,8 +25,24 @@ class Calculator extends Component {
     console.log('set operation');
   }
 
-  updateDisplay = () => {
-    console.log('update display');
+  updateDisplay = value => {
+    let { displayValue } = this.state;
+
+    // prevent multiple occurences of '.'
+    if (value === '.' && displayValue.includes('.')) value = '';
+
+    if (value === 'ce') {
+      // deletes last char in displayValue
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+      // set displayValue to '0' if displayValue is empty string
+      if (displayValue === '') displayValue = '0';
+    } else {
+      // replace displayValue with value if displayValue equal to '0'
+      // else concatenate displayValue and value
+      displayValue === '0' ? displayValue = value : displayValue += value;
+    }
+
+    this.setState({ displayValue });
   }
 
   render = () => {
@@ -47,3 +64,31 @@ class Calculator extends Component {
 }
 
 export default Calculator;
+
+
+/* note* 
+using es5 object method syntax, the method is not bound to the class by default and binding must be declared explicitly in the constructor method,  if you forget to bind this.handleClick and pass it to an onClick handler, "this" will be undefined.
+
+class CalculatorES5 extends Component {
+  constructor(props) {
+    this.state = {
+      displayValue: '0',
+    }
+    // explicit binding
+    this.updateDisplay = this.updateDisplay.bind(this);
+  }
+  updateDisplay(value) {
+    this.setState({ displayValue: value });
+  }
+}
+
+ES6 - no need
+class Calculator extends Component {
+  state = {
+    displayValue: '0',
+  }
+  updateDisplay = value => this.setState({ displayValue: value });
+}
+
+https://reactjs.org/docs/handling-events.html
+*/
