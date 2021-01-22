a9d96bd056b462aeb4e975316bb53d05e25f6f09
Cloudbop <boppincolin@gmail.com>
2021-01-22 23:05:41 +0000

    PASSTEST- Mount component for deep rendering. (fix shallow rendering issue) see cmts

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -17,29 +17,12 @@ describe('Keypad Unit Tests', () => {
   ));
 
   it('Should compare against snapshot <Keypad/>', () => expect(wrapper).toMatchSnapshot())
-  
   it('should render 4 <div />', () => {
     expect(wrapper.find('div').length).toEqual(4);
   });
-
-
-  // start with shallow (shallow render)
-  // Use mount, for lifecyle componentDidMount or componentDidUpdate
-  // DOM rendering, component lifecycle, and the behavior of child components
-  it('renders the values of numbers', () => {
-    wrapper.setProps({numbers: ['0', '1', '2']});
-    expect(wrapper.find('.numbers-container').text()).toEqual('012');
-  });
-  it('renders the values of operators', () => {
-    wrapper.setProps({operators: ['+', '-', '*', '/']});
-    expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
-  });
-
-
   it('should render an instance of the Key component', () => {
     expect(wrapper.find('Key').length).toEqual(1);
   });
-
   // 
   it('(*1) - should render an instance of the Key component for each index of numbers, operators, and the submit Key', () => {
     const numbers = ['0', '1'];
@@ -49,4 +32,43 @@ describe('Keypad Unit Tests', () => {
     wrapper.setProps({ numbers, operators });
     expect(wrapper.find('Key').length).toEqual(keyTotal);
   });
+
+  // start with shallow (shallow render)
+  //  - doesn't render child component content.
+  // it('renders the values of numbers', () => {
+  //   wrapper.setProps({numbers: ['0', '1', '2']});
+  //   expect(wrapper.find('.numbers-container').text()).toEqual('012');
+  // });
+  // it('renders the values of operators', () => {
+  //   wrapper.setProps({operators: ['+', '-', '*', '/']});
+  //   expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+  // });
+
+  // Use mount, for lifecyle componentDidMount or componentDidUpdate
+  // DOM rendering, component lifecycle, and the behavior of child components
+  describe('mounted Keypad', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = mount(
+        <Keypad
+          callOperator={jest.fn()}
+          numbers={[]}
+          operators={[]}
+          setOperator={jest.fn()}
+          updateDisplay={jest.fn()}
+        />
+      );
+    });
+
+    it('renders the values of numbers to the DOM', () => {
+      wrapper.setProps({ numbers: ['0', '1', '2'] })
+      expect(wrapper.find('.numbers-container').text()).toEqual('012');
+    });
+    it('renders the values of operators to the DOM', () => {
+      wrapper.setProps({ operators: ['+', '-', '*', '/'] });
+      expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+    });
+  });
+
 });
