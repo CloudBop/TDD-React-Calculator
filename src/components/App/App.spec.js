c3b246b3d62250adb680c06ac2380afa5a84974c
Cloudbop <boppincolin@gmail.com>
2021-01-22 16:25:24 +0000

    added snapshot4 App+Calculator + failing Calculator test for Display

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -9,7 +9,7 @@ describe('App', () => {
   beforeEach(() => wrapper = shallow(<App />));
 
   // add a snapshot tests as the first test in each spec file. This creates a pattern of placing the generic tests (snapshot, shallow render) that appear in each spec file above the component specific tests
-  it('Should compare against snapshot', () => expect(wrapper).toMatchSnapshot())
+  it('Should compare against snapshot <App/>', () => expect(wrapper).toMatchSnapshot())
   // similar to above but no shapshot
   // it('should render a <div />', () => {
   //   expect(wrapper.find('div').length).toEqual(1);
