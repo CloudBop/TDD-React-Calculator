e9671b510c132969ffbffb71d489caffd2e0f8ac
Cloudbop <boppincolin@gmail.com>
2021-01-22 18:01:21 +0000

    added CSS+ snapshot for  <Dispay/>

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Display from './Display'
 
-describe('Calculator', () => {
+describe('Display Unit Tests', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<Display displayValue={""} />));
-  // it('Should compare against snapshot <Calculator/>', () => expect(wrapper).toMatchSnapshot())
+  it('Should compare against snapshot <Display/>', () => expect(wrapper).toMatchSnapshot())
   
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
